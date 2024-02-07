ac26b25a230cbb47d325ae35652ae7d6d86b5047
arafat <bsse1435@iit.du.ac.bd>
2024-02-07 09:03:57 +0600

    CSE-401 Lab01 updated

---
+++ CSE-401/Lab01/Lab_02_1435.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+path=$1
+
+if [[ ! -d "$path" ]]; then
+	echo "This is not a directory"
+	exit 1
+fi
+
+# all_files=$(find "$path" -type f -name "*.c")
+# echo "$all_files"
+
+
+
+echo "filename     size     lineOfCode"
+echo "#################################"
+for file in `find $path -type f -name "*.c" -or -name "*.cpp"` ; do
+	filename=$(basename -- "$file")
+	size=$(stat --format=%s "$file")
+	lineOfCode=$(wc -l < "$file")
+
+	echo "$filename     $size Bytes     $lineOfCode"
+done
