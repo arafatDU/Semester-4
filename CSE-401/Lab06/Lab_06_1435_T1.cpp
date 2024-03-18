7681f394374e13d7b5d34a28f61aa8d599b40775
ARAFAT <munshiarafathussain@gmail.com>
2024-03-18 19:29:06 +0600

    Lab06 updated

--- CSE-401/Lab06/Lab_06_1435_T1.cpp
+++ CSE-401/Lab06/Lab_06_1435_T1.cpp
@@ -1,56 +1,88 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define N 5
+#define N 6
+
+struct Process { 
+    int pid; 
+    int burstT;
+    int arrivalT;
+}; 
+
 
 int main()
 {
-    int process[N] = {1, 2, 3, 4, 5};
-    int arivalT[N] = {0, 0, 0, 0, 0};
-    int burstT[N] = {2, 1, 8, 4, 5};
-    int priority[N] = {2, 1, 4, 2, 3};
+    int process[N] = {1, 2, 3, 4, 5, 6};
+    int arivalT[N] = {0, 25, 30, 60, 100, 105};
+    int burstT[N] = {20, 25, 25, 15, 10, 10};
+    int priority[N] = {40, 30, 30, 35, 5, 10};
 
     int completeT[N], turnaroundT[N], waitingT[N], sum=0;
     float  avgTT=0, avgWT=0;
-    vector< pair <int,int> > bTpair;
-    vector< pair <int,int> > cTpair;
     
 
+    Process Table[N];
     for(int i=0; i<N; i++){
-        bTpair.push_back( make_pair((burstT[i]), i) );
-    }
-    sort(bTpair.begin(), bTpair.end());
-    cout<<"-------------TEST---------------"<<endl;
-    for(int i=0; i<N; i++) cout<<"burstT: "<<bTpair[i].first<< "   index: "<<bTpair[i].second<<endl;
-    cout<<"-------------------------------\n\n"<<endl;
-
-
-    for(int i=0; i<N; i++){
-        sum += bTpair[i].first;
-        completeT[bTpair[i].second] = sum;
-        turnaroundT[bTpair[i].second] = completeT[bTpair[i].second] - arivalT[bTpair[i].second];
-        waitingT[bTpair[i].second] = turnaroundT[bTpair[i].second] - burstT[bTpair[i].second];
+        Table[i].pid = process[i];
+        Table[i].burstT = burstT[i];
+        Table[i].arrivalT = arivalT[i];
     }
 
 
+    int remainT[n]; 
+  
+    
+    for (int i = 0; i < n; i++) 
+        remainT[i] = Table[i].bt; 
+  
+    int complete = 0, t = 0, minm = INT_MAX; 
+    int shortest = 0, finish_time; 
+    bool check = false; 
+  
+    
+    while (complete != n) { 
 
+        for (int j = 0; j < n; j++) { 
+            if ((Table[j].art <= t) && 
+            (remainT[j] < minm) && remainT[j] > 0) { 
+                minm = remainT[j]; 
+                shortest = j; 
+                check = true; 
+            } 
+        } 
+  
+        if (check == false) { 
+            t++; 
+            continue; 
+        } 
+        remainT[shortest]--; 
+        minm = remainT[shortest]; 
+        if (minm == 0) 
+            minm = INT_MAX; 
 
-    for(int i=0; i<N; i++){
-        cTpair.push_back( make_pair(completeT[i], i) );
+        if (remainT[shortest] == 0) { 
+            complete++; 
+            check = false; 
+
+            finish_time = t + 1; 
+            waitingT[shortest] = finish_time - 
+                        Table[shortest].bt - 
+                        Table[shortest].art; 
+  
+            if (waitingT[shortest] < 0) 
+                waitingT[shortest] = 0; 
+        } 
+        t++; 
     }
-    sort(cTpair.begin(), cTpair.end());
 
-    // printing Gantt chart
-    for(int i=0; i<N; i++) printf("|--- P%d ---|", cTpair[i].second+1);
-    printf("\n0");
-    for(int i=0; i<N; i++) printf("          %d", cTpair[i].first);
-    printf("\n");
+
+
 
 
     // printing table
     printf("\nProcess   A.T    B.T    Priority  C.T   T.T    W.T\n");
     for(int i=0; i<N; i++){
-        printf("   P%d\t   %d\t   %d\t    %d\t   %d\t  %d\t %d\t\n", process[i], arivalT[i], burstT[i], priority[i], completeT[i] ,turnaroundT[i], waitingT[i]);
+        printf("   P%d\t   %d\t   %d\t    %d\t   %d\t  %d\t %d\t\n", Tableess[i], arivalT[i], burstT[i], priority[i], completeT[i] ,turnaroundT[i], waitingT[i]);
     }
 
 
---
+++ CSE-401/Lab06/Lab_06_1435_T2.cpp
@@ -0,0 +1,94 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define N 6
+
+
+void sortPriorityPair(vector<pair<int, int>> &pPair, int *burstT){
+    for(int i=0; i<N; i++){
+        for(int j=i; j<N; j++){
+            pair<int, int> temp;
+            if(pPair[j].first > pPair[i].first){
+                temp.first = pPair[j].first;
+                temp.second = pPair[j].second;
+                pPair[j].first = pPair[i].first;
+                pPair[j].second = pPair[i].second;
+                pPair[i].first = temp.first;
+                pPair[i].second = temp.second;
+            }else if(pPair[j].first == pPair[i].first){
+                if(burstT[pPair[j].second] < burstT[pPair[i].second]){
+                    temp.first = pPair[j].first;
+                    temp.second = pPair[j].second;
+                    pPair[j].first = pPair[i].first;
+                    pPair[j].second = pPair[i].second;
+                    pPair[i].first = temp.first;
+                    pPair[i].second = temp.second;
+                }
+            }
+            
+        }
+    }
+}
+
+int main()
+{
+        int process[N] = {1, 2, 3, 4, 5, 6};
+    int arivalT[N] = {0, 25, 30, 60, 100, 105};
+    int burstT[N] = {20, 25, 25, 15, 10, 10};
+    int priority[N] = {40, 30, 30, 35, 5, 10};
+
+    int completeT[N], turnaroundT[N], waitingT[N], sum=0;
+    float  avgTT=0, avgWT=0;
+    vector< pair <int,int> > pPair;
+    vector< pair <int,int> > cTpair;
+
+    for(int i=0; i<N; i++){
+        pPair.push_back( make_pair(priority[i], i ));
+    }
+    //sort(pPair.begin(), pPair.end());
+    sortPriorityPair(pPair, burstT);
+
+    cout<<"-------------TEST---------------"<<endl;
+    for(int i=0; i<N; i++) cout<<"priority: "<<pPair[i].first<< "   index: "<<pPair[i].second<<endl;
+    cout<<"-------------------------------\n\n"<<endl;
+
+
+    for(int i=0; i<N; i++){
+        sum += arivalT[pPair[i].second] + burstT[pPair[i].second];
+        completeT[pPair[i].second] = sum;
+        turnaroundT[pPair[i].second] = completeT[pPair[i].second] - arivalT[pPair[i].second];
+        waitingT[pPair[i].second] = turnaroundT[pPair[i].second] - burstT[pPair[i].second];
+    }
+
+
+
+
+    for(int i=0; i<N; i++){
+        cTpair.push_back( make_pair(completeT[i], i) );
+    }
+    sort(cTpair.begin(), cTpair.end());
+
+    // printing the Gnatt chart
+    for(int i=0; i<N; i++) printf("|--- P%d ---|", cTpair[i].second+1);
+    printf("\n0");
+    for(int i=0; i<N; i++) printf("          %d", cTpair[i].first);
+    printf("\n");
+
+
+    // printing the table
+    printf("\nProcess   A.T    B.T    Priority  C.T    T.T    W.T\n");
+    for(int i=0; i<N; i++){
+        printf("   P%d\t   %d\t   %d\t    %d\t   %d\t  %d\t %d\t\n", process[i], arivalT[i], burstT[i], priority[i], completeT[i] ,turnaroundT[i], waitingT[i]);
+    }
+
+
+    // calculating average
+    for(int i=0; i<N; i++){
+        avgTT += turnaroundT[i];
+        avgWT += waitingT[i];
+    }
+    printf("\nAverage Turnaround Time = %f\n", avgTT/N);
+    printf("\nAverage Waiting Time = %f\n", avgWT/N);
+
+    return 0;
+}
